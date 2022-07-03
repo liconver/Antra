0b07786041a8ee1ab0dc8259ab65410aa45fb420
liconver <liamc@vt.edu>
2022-07-03 05:01:04 -0400

    changed a function and added questions in comments

--- JS/assignment.js
+++ JS/assignment.js
@@ -1,34 +1,39 @@
- //1
+ //1. Write a JavaScript function that reverse a number. 
  function _1(num) {
     return parseInt(num.toString().split('').reverse().join(''));
 } 
 console.log(_1(32243));
 
-//2
+//2. Write a JavaScript function that checks whether a passed string is palindrome or not? 
 function _2(word) {
     return word.split('').reverse().join('') == word;
 }
 console.log(_2('madam'));
 
-//3
+//3. Write a JavaScript function that generates all combinations of a string. 
 function _3(string) {
     let output = [];
-    for(let i=0; i< string.length; i++){
-        for(let j= i+1; j<string.length + 1; j++){
-            output.push(string.slice(i,j));
+    let temp = [];
+    for (let char of string){
+        temp = [char];
+        let index = 0;
+        while(output[index]){
+            temp.push(''+output[index]+char);
+            index++;
         }
+        output = output.concat(temp);
     }
     return output;
 }
 console.log(_3('dog'));
 
-//4
+//4. Write a JavaScript function that returns a passed string with letters in alphabetical order.
 function _4(string) {
     return string.split('').sort().join('');
 }
 console.log(_4('webmaster'));
 
-//5
+//5. Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string 
 function _5(string) {
     let words = string.split(' ');
 
@@ -38,7 +43,7 @@ function _5(string) {
 }
 console.log(_5('the quick brown fox'));
 
-//6
+//6. Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
 function _6(string) {
     let words = string.split(' ');
     let longest = 0;
@@ -53,7 +58,7 @@ function _6(string) {
 }
 console.log(_6('Web Development Tutorial'));
 
-//7
+//7. Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string. 
 function _7(string) {
     let vowels = new Set(['a','e','i','o','u']);
     let chars = string.split('');
@@ -67,7 +72,7 @@ function _7(string) {
 }
 console.log(_7('the quick brown fox'));
 
-//8
+//8. Write a JavaScript function that accepts a number as a parameter and check the number is prime or not
 function _8(number) {
     if (number>1) {
         for (let i = 2, j = Math.sqrt(number); i <= j; i++) {
@@ -80,13 +85,13 @@ function _8(number) {
 }
 console.log(_8(719));
 
-//9
+//9. Write a JavaScript function which accepts an argument and returns the type. 
 function _9(type) {
     return typeof(type);
 }
 console.log(_9('abc'));
 
-//10
+//10. Write a JavaScript function which returns the n rows by n columns identity matrix. 
 function _10(n) {
     let matrix = [];
     for(let i=0; i <n;i++){
@@ -103,7 +108,7 @@ function _10(n) {
 }
 console.log(_10(5));
 
-//11
+//11. Write a JavaScript function which will take an array of numbers stored and find the second lowest and second greatest numbers, respectively. 
 function _11(array) {
     array.sort((a, b) => a - b);
     let output = [];
@@ -119,7 +124,7 @@ function _11(array) {
 }
 console.log(_11([1,2,3,4,5]));
 
-//12
+//12. Write a JavaScript function which says whether a number is perfect. 
 function _12(number) {
     let sum =0;
     for (let i=1; i<number;i++){
@@ -135,7 +140,7 @@ function _12(number) {
 }
 console.log(_12(28));
 
-//13
+//13. Write a JavaScript function to compute the factors of a positive integer.
 function _13(positive_integer) {
     let factors = [];
     for(let i=1; i<=positive_integer;i++){
@@ -147,7 +152,7 @@ function _13(positive_integer) {
 }
 console.log(_13(6));
 
-//14 
+//14. Write a JavaScript function to convert an amount to coins.  
 function _14(amount, coin_array) {
     coin_array.sort((a, b) => b - a);
     let sum = 0;
@@ -164,14 +169,14 @@ function _14(amount, coin_array) {
     }
     return output;
 }
-//console.log(_14(46, [25, 10, 5, 2, 1]));
+console.log(_14(46, [25, 10, 5, 2, 1]));
 
-//15
+//15. Write a JavaScript function to compute the value of bn where n is the exponent and b is the bases. Accept b and n from the user and display the result. 
 function _15(b, n) {
     return Math.pow(b,n);
 }
 
-//16
+//16. Write a JavaScript function to extract unique characters from a string. 
 function _16(string){
     unique = new Set();
     chars= string.split('');
@@ -182,7 +187,7 @@ function _16(string){
 }
 console.log(_16('thequickbrownfoxjumpsoverthelazydog'));
 
-//17
+//17. Write a JavaScript function to get the number of occurrences of each letter in specified string. 
 function _17(string){
     chars= string.split('');
     letter_count = new Map();
@@ -193,7 +198,7 @@ function _17(string){
 }
 console.log(_17('thequickbrownfoxjumpsoverthelazydog'));
 
-//18
+//18. Write a function for searching JavaScript arrays with a binary search. 
 function _18(value, array) {
     let start = 0;
     let end = array.length-1;
@@ -211,7 +216,7 @@ function _18(value, array) {
 }
 console.log(_18(5,[25, 10, 5, 2, 1]));
 
-//19
+//19. Write a JavaScript function that returns array elements larger than a number. 
 function _19(number, array) {
     return array.filter( (el) => {
         return el > number;
@@ -219,7 +224,7 @@ function _19(number, array) {
 }
 console.log(_19(7,[25, 10, 5, 2, 1]));
 
-//20
+//20. Write a JavaScript function that generates a string id (specified length) of random characters. 
 function _20(number) {
     let output = '';
     let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -231,7 +236,7 @@ function _20(number) {
 }
 console.log(_20(7));
 
-//21
+//21. Write a JavaScript function to get all possible subset with a fixed length (for example 2) combinations in an array. 
 function _21(array, size) {
     let output = [];
     for(let i=0; i< Math.pow(2, array.length); i++) {
@@ -250,7 +255,7 @@ function _21(array, size) {
 }
 console.log(_21([1, 2, 3], 2));
 
-//22
+//22. Write a JavaScript function that accepts two arguments, a string and a letter and the function will count the number of occurrences of the specified letter within the string. 
 function _22(string, letter) {
     chars= string.split('');
     let count =0;
@@ -263,7 +268,7 @@ function _22(string, letter) {
 }
 console.log(_22('microsoft.com','o'));
 
-//23
+//23. Write a JavaScript function to find the first not repeated character. 
 function _23(string) {
     for (let i=0; i<string.length; i++) {
         let char = string.charAt(i);
@@ -275,7 +280,7 @@ function _23(string) {
 }
 console.log(_23('abacddbec'));
 
-//24
+//24. Write a JavaScript function to apply Bubble Sort algorithm. 
 function _24(array) {
     for (let i = 0; i < array.length; i++) {
 		for (let j = 0; j < array.length; j++) {
@@ -290,14 +295,14 @@ function _24(array) {
 }
 console.log(_24([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]));
 
-//25
+//25. Write a JavaScript function that accept a list of country names as input and returns the longest country name as output. 
 function _25(list){
     list.sort((a,b) => b.length - a.length);
     return list[0];
 }
 console.log(_25(["Australia", "Germany", "United States of America"]));
 
-//26
+//26. Write a JavaScript function to find longest substring in a given a string without repeating characters. 
 function _26(string){
    let s = '';
    let substr = '';
@@ -319,7 +324,7 @@ function _26(string){
 }
 console.log(_26('microsoft.com'));
 
-//27
+//27. Write a JavaScript function that returns the longest palindrome in a given string. 
 function _27(string) {
     let start;
     let maxLength;
@@ -338,13 +343,13 @@ function _27(string) {
     return string.substring(start, maxLength-1);
 }
 
-//28
+//28. Write a JavaScript program to pass a 'JavaScript function' as parameter. 
 function _28(func){
     let output = func(argument);
     console.log(output)
 }
 
-//29
+//29. Write a JavaScript function to get the function name.
 function _29(func){
     return func.name;
 }
